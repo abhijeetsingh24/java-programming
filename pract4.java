9d92b7d7beda1196f0a6ae53a4dceea63f1db40a
abhijeetsingh24 <78357616+abhijeetsingh24@users.noreply.github.com>
2022-06-13 23:01:00 +0530

    Add files via upload
    java program to print all n even numbers

---
+++ pract4.java
@@ -0,0 +1,18 @@
+package abc;
+
+import java.util.Scanner;
+
+public class pract4 {
+
+	public static void main(String[] args) {
+		//print all n even number
+		Scanner sc=new Scanner(System.in);
+		int n =sc.nextInt();
+		for(int i=2;i<n;i=i+2) {
+			System.out.println(i);
+		}
+	  
+
+	}
+
+}
